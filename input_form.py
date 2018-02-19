1a1162669033c043453808b63a0a21620b5f62b1
gaizeror <gaizeror@gmail.com>
2018-02-19 15:52:00 +0200

    first form

---
+++ input_form.py
@@ -0,0 +1,4 @@
+team = input("Please enter the team's name ")
+application = input("What is the application name? ")
+service = input("What is the service name? ")
+port = input("What is the port your service is listening? ")
